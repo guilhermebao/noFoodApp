0cee276d086807e76ff3f3cb76bff4bb1e147a26
Guilherme Carvalho Báo <guilhermecarvalhobao@gmail.com>
2019-01-22 21:50:03 -0200

    Instação do mongoose e conexão com o com o banco de dados

--- Nofood/bin/configuration/variables.js
+++ Nofood/bin/configuration/variables.js
@@ -1,6 +1,9 @@
 const variables = {
     Api: {
         port: process.env.port || 3000
+    },
+    Database: {
+        connection: process.env.connection || 'mongodb://admin:nofood123456@ds119772.mlab.com:19772/nofood'
     }
 }
 module.exports = variables;
--- Nofood/bin/express.js
+++ Nofood/bin/express.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const variables = require('../bin/configuration/variables');
 
 //Rotas
 const categoriaRouter = require('../routes/categoria-router');
@@ -11,6 +13,9 @@ const app = express();
 //Configuração de parse do JSON
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+//Configurando a conexao com o banco de dados
+mongoose.connect(variables.Database.connection);
 
 //Configurando rotas
 app.use('/api/categoria', categoriaRouter);
